bd8ca11dd4a3dc252b0b40d944ea913a0e06fe9c
Facundo Robert <robert.facundo@hotmail.com>
2025-03-20 16:56:08 -0300

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-//!!!!!!!!!!
-  app.enableCors();
+  //!!!!!!!!!!
+  app.enableCors({
+    origin: 'https://frontend-three-iota-40.vercel.app', // Permite solicitudes de este dominio
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log('Application is running...', 'Bootstrap');
 }
