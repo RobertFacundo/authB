374a0cf8c4579b3693f538d8c794d8456bf78a0c
Facundo Robert <robert.facundo@hotmail.com>
2025-03-24 13:45:04 -0300

    backend message

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,14 +4,14 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     return `
-          <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
-              <p>⚠️ <strong>This backend is hosted on Render</strong> and may take a few seconds to respond due to cold start.</p>
-              <p>Now that we know for sure that the backend is awake, you can access the website:</p>
+           <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+              <p>⚠️ <strong>This backend is hosted for FREE on Render</strong> and may take a few seconds to respond due to cold start.</p>
+              <p>Now that we know the backend is up and running, feel free to visit the website:</p>
               <a href="https://authf-yama.vercel.app/" target="_blank" style="font-size: 18px; color: blue; text-decoration: none;">
                 👉 Go to the website
               </a>
-              <p>Thank You for understanding</p>
-          </div>
+              <p>Thank you for your patience and understanding!</p>
+            </div>
     `
   }
 }
