535e98f8717059005f9b21c1fa250dc63cc33bff
Facundo Robert <robert.facundo@hotmail.com>
2025-03-24 13:22:08 -0300

    coldstartrender

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '  Este backend está alojado en Render y puede tardar unos segundos en responder debido al cold start. Ahora que el backend esta despierto puedes ingresar al website => https://authf-yama.vercel.app/';
   }
 }
