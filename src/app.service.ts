98a43a6bc3943c5bf0a43c10482126237b07b014
Facundo Robert <robert.facundo@hotmail.com>
2025-03-24 13:37:56 -0300

    Backend Message

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,10 +6,11 @@ export class AppService {
     return `
           <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
               <p>⚠️ <strong>This backend is hosted on Render</strong> and may take a few seconds to respond due to cold start.</p>
-              <p>Now that the backend is awake, you can access the website:</p>
+              <p>Now that we know for sure that the backend is awake, you can access the website:</p>
               <a href="https://authf-yama.vercel.app/" target="_blank" style="font-size: 18px; color: blue; text-decoration: none;">
                 👉 Go to the website
               </a>
+              <p>Thank You for understanding</p>
           </div>
     `
   }
