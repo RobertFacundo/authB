1cb5b6a8507574ff8ef5f5b1077828fb521c6838
Facundo Robert <robert.facundo@hotmail.com>
2025-03-24 15:07:36 -0300

    PostgreSql DataBase connected to project and added production and development environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (ConfigService: ConfigService) => ({
+        type: 'postgres',
+        host: ConfigService.get<string>('DB_HOST'),
+        port: ConfigService.get<number>('DB_PORT'),
+        username: ConfigService.get<string>('DB_USERNAME'),
+        password: ConfigService.get<string>('DB_PASSWORD'),
+        database: ConfigService.get<string>('DB_DATABASE'),
+        entities: [],
+        logging: process.env.NODE_ENV !== 'production', 
+        synchronize: process.env.NODE_ENV !== 'production',
+      }),
+      inject:[ConfigService]
+    }),
+    TypeOrmModule.forFeature([])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //!!!!!!!!!!!!
+
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
-  console.log('Application is running...', 'Bootstrap');
+
+  if (process.env.NODE_ENV === 'production') {
+    console.log('Application is running in ... production environment');
+  } else {
+    console.log('Application is running in ...development environment');
+  }
 }
 bootstrap();
