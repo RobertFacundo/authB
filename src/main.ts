317506952fdb8ba9fec3d92216000e4802d09091
Facundo Robert <robert.facundo@hotmail.com>
2025-03-28 20:02:29 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-  const port = process.env.PORT || 3000; 
+  app.enableCors({
+    origin: ['https://authf-yama.vercel.app'],  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true,
+  });
+  const port = process.env.PORT || 3000;
 
   await app.listen(port);
 
