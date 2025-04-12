c294c56c7482d91ab551581c209a6f823d9b37e6
Facundo Robert <robert.facundo@hotmail.com>
2025-04-12 15:12:20 -0300

    !

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-    },
-    moduleNameMapper: {
-      '^src/(.*)$': '<rootDir>/$1',
-    },
-    collectCoverageFrom: ['**/*.(t|j)s'],
-    coverageDirectory: '../coverage',
-    testEnvironment: 'node',
-  };
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'babel-jest',
+    '^.+\\.tsx$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@nestjs/common).+\\.js$" // Asegúrate de que Jest no ignore los módulos de NestJS
+  ],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
